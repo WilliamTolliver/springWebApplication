6abe60541c1cc0071e6b3837f5737ec096a5768d
William_R_Tolliver@yahoo.com <William_R_Tolliver@yahoo.com>
2018-02-04 04:10:20 -0500

    Translating Hibernate exceptions to Spring exceptions 	- Add org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor to bean Profiles(dev and production) 	- Marked DAO classes as repositories

--- src/com/wttechnologies/spring/web/dao/UserDAO.java
+++ src/com/wttechnologies/spring/web/dao/UserDAO.java
@@ -7,15 +7,16 @@ import javax.sql.DataSource;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Component("userDAO")
+@Repository
 public class UserDAO {
 
 	// Properties
