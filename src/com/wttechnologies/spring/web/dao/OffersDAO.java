2d407555bd0edea10131538da5456cf5ec20f1f8
William_R_Tolliver@yahoo.com <William_R_Tolliver@yahoo.com>
2018-02-04 04:11:36 -0500

    Forgot to add repository to OfferDAO

--- src/com/wttechnologies/spring/web/dao/OffersDAO.java
+++ src/com/wttechnologies/spring/web/dao/OffersDAO.java
@@ -1,22 +1,21 @@
 package com.wttechnologies.spring.web.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component("offersDAO")
+@Repository
 public class OffersDAO {
 
 	// Properties
